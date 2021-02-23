c57b909ad3e95b7d8336fe485314126085d25f08
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-02-23 17:24:08 -0300

    YW PAR(p)-A funcionando

--- parpa/yulewalker.py
+++ parpa/yulewalker.py
@@ -129,12 +129,12 @@ class YuleWalkerPAR:
 
 class YuleWalkerPARA:
     """
+    Conjunto de métodos para cálculos associados com a
+    montagem da matriz de autocovariâncias de YW.
     """
     def __init__(self,
-                 sinal: np.ndarray,
-                 ordens: List[int]):
-        n = len(sinal)
-        self.ordens = ordens
+                 sinal: np.ndarray):
+
         self.sinal = deepcopy(sinal)
         self.n_amostras, self.periodos = self.sinal.shape
         self.medias = self._medias_per(sinal)
@@ -151,128 +151,203 @@ class YuleWalkerPARA:
         for j in range(self.periodos):
             media = np.mean(self.medias[1:, j])
             desvio = np.std(self.medias[1:, j], ddof=self.ddof)
-            self.medias[:, j] = (self.medias[:, j] - media) / desvio
+            self.medias[1:, j] = (self.medias[1:, j] - media) / desvio
 
     def _medias_per(self , sinal: np.ndarray):
         """
         Calcula a média dos últimos períodos e retorna a matriz
         de médias.
+
+        OBS: Pela forma como é calculada a matriz de médias, a
+        entrada `a(i, j)` contém as médias dos 12 períodos anteriores
+        a ela, sem considerar a mesma. Ou seja, Um lag `-1` na
+        função de covariância segundo o CEPEL significa um lag
+        `0` na função de covariância deste código.
         """
         # Coloca todos os dados em sequência para facilitar
         sinal_seq = np.array(sinal.reshape(sinal.size,))
         medias = np.zeros((self.n_amostras, self.periodos))
-        for i in range(1, 88):
-            for j in range(12):
-                i_sinal = (i - 1) * 12 + j
-                f_sinal = i_sinal + 12
+        for i in range(1, self.n_amostras):
+            for j in range(self.periodos):
+                i_sinal = (i - 1) * self.periodos + j
+                f_sinal = i_sinal + self.periodos
                 medias[i, j] = np.mean(sinal_seq[i_sinal:f_sinal])
         return medias
 
-    def _autocorr(self, p: int, lag: int) -> float:
-        sinal_m = self.sinal[:, p]
-        sinal_lag = self.sinal[:, p - lag]
+    def _autocov(self, p: int, lag: int) -> float:
+        """
+        Calcula a autocovariância para o sinal normalizado, em
+        um período `p` e com lag `lag`. A autocovariância é
+        definida em [1] por:
+
+        Gamma(k) = E[(z(t) - E[z(t)]) * (z(t - k) - E[z(t - k)])]
+
+        Para o modelo periódico, convém representar o período de
+        cada amostra. Se o total de períodos é P::
+
+        Gamma(p, k) = E[(z(t, p) - E[z(t, p)]) * z(t - k, K) - E[z(t - k, K)])]
+
+        onde K = (p - k) % P.
+
+        **Referências**
+
+        [1] K.W. Hipel A.I McLeod. Time Series Modelling of Water Resources
+        and Environmental Systems, Volume 45, Cap. 14. 1994.
+
+        """
+        lag_mod = lag % self.periodos
+        sinal_m = deepcopy(self.sinal[:, p])
+        sinal_lag = deepcopy(self.sinal[:, p - lag_mod])
         # Se o mês de referência para o cálculo das
         # autocorrelações, com o lag "volta um ano",
         # então descontamos uma amostra de cada.
-        if p < lag:
-            sinal_m = sinal_m[1:len(sinal_m)]
-            sinal_lag = sinal_lag[:len(sinal_lag)-1]
-        u = np.mean(np.multiply(sinal_m,
-                                sinal_lag))
-        ac = (u / (np.std(sinal_m,
-                          ddof=self.ddof) *
-                   np.std(sinal_lag,
-                          ddof=self.ddof)))
-        return ac
+        if p < lag_mod:
+            sinal_m = sinal_m[1:]
+            sinal_lag = sinal_lag[:-1]
+        u = (1.0 / self.n_amostras) * np.sum(np.multiply(sinal_m,
+                                                         sinal_lag))
+        return u
 
-    def _autocorr_media(self, p: int, lag: int) -> np.ndarray:
+    def _cov_media(self, p: int, lag: int) -> float:
+        """
+        Realiza o cálculo da covariância entre o sinal defasado no
+        tempo com a componente da média anual.
+        
+        OBS: Pela forma como é calculada a matriz de médias anuais,
+        a variável `A(t - 1)`, como no relatório do CEPEL, que da
+        origem ao termo `RHO(m - 1)` está associada a um lag `m`
+        nesta função.
+
+        Segue o mesmo princípio da autocovariância, porém realizando
+        o produto dos dois sinais em questão ao invés do mesmo sinal
+        com lag.
+        """
         lag_mod = lag % self.periodos
-        sinal_med = self.medias[:, p]
-        sinal_lag = self.sinal[:, lag_mod]
-        print(f"AUTOCOR MEDIA: p = {p}, lag = {lag}, col = {lag_mod}")
+        sinal_med = deepcopy(self.medias[:, p])
+        sinal_lag = deepcopy(self.sinal[:, p - lag_mod])
         # Se o mês de referência para o cálculo das
         # autocorrelações com o lag "volta um ano",
         # então descontamos uma amostra de cada.
         if p < lag_mod:
-            sinal_med = sinal_med[1:self.n_amostras]
-            sinal_lag = sinal_lag[:self.n_amostras-1]
+            sinal_med = sinal_med[1:]
+            sinal_lag = sinal_lag[:-1]
+        u = (1.0 / self.n_amostras) * np.sum(np.multiply(sinal_med,
+                                                         sinal_lag))
+        return u
 
-        u = np.mean(np.multiply(sinal_med,
-                                sinal_lag))
-        ac = (u / (np.std(sinal_med,
-                          ddof=self.ddof) *
-                   np.std(sinal_lag,
-                          ddof=self.ddof)))
-        return ac
+    def _matriz_extendida(self, p: int, lag: int) -> np.ndarray:
+        """
+        Retorna a matriz extendida que será usada para montar
+        o sistema de equações de Yule-Walker para um período
+        `p` e um lag máximo de `lag`.
+        """
+        ORDEM_MATRIZ = lag + 2
+        # Monta a matriz YW
+        mat = np.ones((ORDEM_MATRIZ, ORDEM_MATRIZ))
+        for i in range(ORDEM_MATRIZ - 1):
+            for j in range(i + 1, ORDEM_MATRIZ - 1):
+                m = (p - i) % self.periodos
+                lag = j - i
+                mat[i, j] = self._autocov(m, lag)
+                mat[j, i] = mat[i, j]
+        # Obtém os coeficientes
+        # Adiciona os termos da média do período
+        for i in range(ORDEM_MATRIZ - 1):
+            mat[ORDEM_MATRIZ - 1, i] = self._cov_media(p, i)
+            mat[i, ORDEM_MATRIZ - 1] = mat[ORDEM_MATRIZ - 1, i]
 
-    def estima_modelo(self) -> List[List[float]]:
-        # Para cada período, obtém os coeficientes
+        return mat
+
+    def _resolve_yw(self,
+                    mat: np.ndarray,
+                    p: int,
+                    lag: int) -> List[float]:
+        """
+        Monta e resolve o sistema de equações de Yule-Walker
+        a partir de uma matriz extendida, para um certo período
+        `p` e um lag `lag`. 
+        """
+        ORDEM_MATRIZ = lag + 2
+        ind_submat = list(range(1, ORDEM_MATRIZ))
+        rho_grid = np.ix_(ind_submat, [0])
+        yw_grid = np.ix_(ind_submat, ind_submat)
+
+        yw = mat[yw_grid]
+        rho = mat[rho_grid]
+        coefs_norm = list(np.matmul(np.linalg.inv(yw), rho))
+        return [c[0] for c in coefs_norm]
+
+    def estima_modelo(self,
+                      ordens: List[float]) -> List[List[float]]:
+        """
+        Realiza a estimação do modelo `PAR-A(p1, p2, ..., pm)`
+        resolvendo o sistema de equações de Yule-Walker para a
+        lista de ordens `[p1, p2, ..., pm]` fornecidas.
+        """
         coefs: List[List[float]] = []
         for p in range(self.periodos):
-            o = self.ordens[p]
-            # Monta a matriz YW
-            yw = np.ones((o + 1, o + 1))
-            for i in range(o):
-                for j in range(o):
-                    m = 0
-                    lag = 0
-                    if j < i:
-                        m = (p - j - 1) % self.periodos
-                        lag = i - j
-                    elif j > i:
-                        m = (p - i - 1) % self.periodos
-                        lag = j - i
-                    yw[i, j] = self._autocorr(m, lag)
-            # Obtém os coeficientes
-            # Adiciona os termos da média do período
-            for i in range(o):
-                yw[o, i] = self._autocorr_media(p - 1, i)
-                yw[i, o] = yw[o, i]
-            # Obtém os coeficientes
-            media_futura = self._autocorr_media(p - 1, -1)
-            autocors = np.concatenate([[self._autocorr(p, lag)
-                                        for lag in range(1, o + 1)],
-                                      np.array([media_futura])]).T
-            coefs_norm = list(np.matmul(np.linalg.inv(yw), autocors))
-            coefs.append(coefs_norm)
+            o = ordens[p]
+            mat = self._matriz_extendida(p, o)
+            coefs.append(self._resolve_yw(mat, p, o))
+
         return coefs
 
     def facp(self, p: int, maxlag: int) -> List[float]:
+        """
+        Determina a Função de Autocorrelação Parcial (FACP)
+        através do método das correlações condicionadas para
+        um período `p` e lags de 1 a `maxlag`.
+
+        O método consiste em dividir a matriz de covariâncias
+        em quatro submatrizes:
+
+        SIGMA = [[SIG11, SIG12],
+                 [SIG21, SIG22]]
+        onde SIG11 e SIG22 são quadradas e SIG12^T = SIG21.
+
+        SIG11 é a matriz de covariância entre `z(t)` e `z(t - k)`
+
+        SIG22 é a matriz de covariâncias de `[z(t - 1), ... z(t - k + 1), m]`,
+        onde `m` é a média anual do período anterior.
+
+        SIG12 é a matriz de covariâncias entre `[z(t), z(t - k)]` e
+        `[z(t - 1), ... z(t - k + 1), m]`.
+
+        A matriz de covariância condicionada é dada por:
+
+        COND = SIG11 - SIG12 * SIG22^(-1) * SIG21
+
+        A FACP é obtida na entrada [0, 1] ou [1, 0] de COND.
+        """
         # Para cada período, obtém os coeficientes
-        coefs: List[List[float]] = []
-        # Monta a matriz YW
-        yw = np.ones((maxlag + 1, maxlag + 1))
-        for i in range(maxlag):
-            for j in range(maxlag):
-                m = 0
-                lag = 0
-                if j < i:
-                    m = (p - j - 1) % self.periodos
-                    lag = i - j
-                elif j > i:
-                    m = (p - i - 1) % self.periodos
-                    lag = j - i
-                yw[i, j] = self._autocorr(m, lag)
-        # Obtém os coeficientes
-        # Adiciona os termos da média do período
-        for i in range(maxlag):
-            yw[maxlag, i] = self._autocorr_media(maxlag - 1, i)
-            yw[i, maxlag] = yw[maxlag, i]
-        # Obtém os coeficientes
-        media_futura = self._autocorr_media(maxlag - 1, -1)
-        autocors = np.concatenate([[self._autocorr(p, lag)
-                                    for lag in range(1, maxlag + 1)],
-                                    np.array([media_futura])]).T
-        coefs_norm = list(np.matmul(np.linalg.inv(yw), autocors))
-        coefs.append(coefs_norm)
         acps: List[float] = []
-        # A FACP(k) é definida como o último coeficiente do modelo
-        # AR de ordem k ajustado via Yule-Walker.
-        # Logo, é da forma:
-        #
-        # Ajuste_YW_ordem1[0], Ajuste_YW_ordem2[1], ...
+        mat = self._matriz_extendida(p, maxlag)
+        # Calcula as correlações condicionais
         for o in range(1, maxlag):
-            acp = list(np.matmul(np.linalg.inv(yw[:o, :o]),
-                                autocors[:o]))[o - 1]
-            acps.append(acp)
+            ind11 = [0, o]
+            ind22 = [[maxlag + 1] if maxlag == 1 else
+                        list(range(1, o)) + [maxlag + 1]][0]
+
+            # Caso de sig22 singular
+            if np.array_equal(ind22,
+                              np.zeros_like(ind22)):
+                phi = mat[0, o + 1] / (np.sqrt(mat[0, 0] *
+                                       mat[o + 1, o + 1]))
+                acps.append(phi)
+                continue
+
+            ix11_grid = np.ix_(ind11, ind11)
+            ix12_grid = np.ix_(ind11, ind22)
+            ix22_grid = np.ix_(ind22, ind22)
+            sig11 = mat[ix11_grid]
+            sig12 = mat[ix12_grid]
+            sig22 = mat[ix22_grid]
+
+            cond = sig11 - np.matmul(np.matmul(sig12,
+                                               np.linalg.inv(sig22)),
+                                     sig12.T)
+            
+            phi = cond[0, 1] / (np.sqrt(cond[0, 0] * cond[1, 1]))
+            acps.append(phi)
+
         return np.array(acps)
--- test.py
+++ test.py
@@ -22,13 +22,13 @@ dir = "/home/rogerio/ONS/validacao_newave2743/pmo_2020_11_parpa"
 parp = LeituraPARp(dir).le_arquivo()
 entrada = parp.series[1][ :, :, 0][:, 1:]
 ordens = parp.ordens[1][0, 1:]
-yw = YuleWalkerPARA(entrada, ordens)
-# print(yw.facp(0, 12))
-coefs = yw.estima_modelo()
-print("Yule-Walker Teste | Arquivo parp.dat")
-coefs_parp = parp.coeficientes[1][:12, :, 0]
-for i, m in enumerate(coefs):
-    ordem = len(m)
-    print(["{:1.3f} | {:1.3f}"
-           .format(m[j], coefs_parp[i][j])
-           for j in range(ordem)])
+yw = YuleWalkerPARA(entrada)
+print(yw.facp(0, 12))
+coefs = yw.estima_modelo(ordens)
+# print("Yule-Walker Teste | Arquivo parp.dat")
+# coefs_parp = parp.coeficientes[1][:12, :, 0]
+# for i, m in enumerate(coefs):
+#     ordem = len(m)
+#     print(["{:1.3f} | {:1.3f}"
+#            .format(m[j], coefs_parp[i][j])
+#            for j in range(ordem)])
